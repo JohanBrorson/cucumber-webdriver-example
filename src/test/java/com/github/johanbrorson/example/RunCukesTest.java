ed988adadf34ea365bd75f52c47e4cc9a6ecee22
Johan Brorson <johbro@spotify.com>
2014-10-09 14:49:05 +0200

    Add format to CucumberOptions

--- src/test/java/com/github/johanbrorson/example/RunCukesTest.java
+++ src/test/java/com/github/johanbrorson/example/RunCukesTest.java
@@ -3,7 +3,8 @@ package com.github.johanbrorson.example;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(features = "src/test/resources/features",
+    format = {"pretty", "json:target/cucumber-report.json", "html:target/cucumber-report"})
 public class RunCukesTest extends AbstractTestNGCucumberTests {
 
 }
